382ccc6c5b256cfbbee2b0ce56670bd7669f5402
Linus Färnstrand <faern@faern.net>
2020-05-03 13:03:45 +0200

    Tidy up a bit

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,17 @@
+use std::time::Duration;
+
+const DELAY: Duration = Duration::from_millis(1);
+
 #[cfg(not(feature = "tokio"))]
 #[async_std::main]
 async fn main() {
-    async_std::task::sleep(std::time::Duration::from_millis(1)).await;
-    println!("Hello from async-std");
+    async_std::task::sleep(DELAY).await;
 }
 
 #[cfg(feature = "tokio")]
 #[tokio::main]
 async fn main() {
-    tokio::time::delay_for(std::time::Duration::from_millis(1)).await;
-    println!("Hello from tokio");
+    tokio::time::delay_for(DELAY).await;
 }
 
 // When using `cargo expand` on the crate these two main methods fall out:
@@ -19,7 +21,7 @@ fn _expanded_main_asyncstd() {
     #[cfg(not(feature = "tokio"))]
     async fn main() {
         {
-            async_std::task::sleep(std::time::Duration::from_millis(1)).await;
+            async_std::task::sleep(DELAY).await;
         }
     }
     async_std::task::block_on(async { main().await })
@@ -35,16 +37,7 @@ fn _expanded_main_tokio() {
         .unwrap()
         .block_on(async {
             {
-                tokio::time::delay_for(std::time::Duration::from_millis(1)).await;
-
-                {
-                    ::std::io::_print(::core::fmt::Arguments::new_v1(
-                        &["Hello from tokio\n"],
-                        &match () {
-                            () => [],
-                        },
-                    ));
-                };
+                tokio::time::delay_for(DELAY).await;
             }
         })
 }
